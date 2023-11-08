c4b5ced005bbd36e892dd9165d7ae417f81432f6
Derek Jiang <derek.l.jiang@gmail.com>
2023-11-08 14:25:05 -0800

    AI FSM Boilerplate
    Basic code for managing AI state.

--- Assets/Scripts/CafeAI/BaseStateMachine.cs
+++ Assets/Scripts/CafeAI/BaseStateMachine.cs
@@ -1,17 +1,23 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 // referenced from:
 // https://www.toptal.com/unity-unity3d/unity-ai-development-finite-state-machine-tutorial
+// TODO: we probably want additional state to be stored inside some monobehavior or data structure
+// We can probably do this by writing a small data container behavior for each NPC type?
 
 namespace CafeAI
 {
     public class BaseStateMachine : MonoBehaviour
     {
         [SerializeField] private BaseState _initialState;
+        private Dictionary<Type, Component> _cachedComponents;
 
         private void Awake()
         {
             CurrentState = _initialState;
+            _cachedComponents = new Dictionary<Type, Component>();
         }
 
         public BaseState CurrentState { get; set; }
@@ -19,6 +25,22 @@ namespace CafeAI
         private void Update()
         {
             CurrentState.Execute(this);
+        }
+        
+        // Overrides default GetComponent to cache
+        // Notes for Derek - new: overrides base GetComponent
+        // Where T: Component: restricts which classes can be passed.
+        public new T GetComponent<T>() where T : Component
+        {
+            if (_cachedComponents.ContainsKey(typeof(T)))
+                return _cachedComponents[typeof(T)] as T;
+            
+            var component = base.GetComponent<T>();
+            if (component != null)
+            {
+                _cachedComponents.Add(typeof(T), component);
+            }
+            return component;
         }
     }
 
