545151079add13aaa031f44d21e4c759d903a3bf
LightenedLimited <macnguyenanh@gmail.com>
2023-11-15 13:43:59 -0800

    added cat animation

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,16 +9,22 @@ public class PlayerController : MonoBehaviour
     Rigidbody rb; 
     private float moveX, moveY;
     private Vector3 jumpDirection; 
-    public float acceleration = 0.0f;
+    public float velocity = 0.0f;
     public float jumpMagnitude = 0.0f;
-    
+    public float rotation_speed = 0.001f;
+    float timeCount = 0.0f;
+
+    public Animator anim;
+
     // Start is called before the first frame update
     void Start()
     {
         moveX = 0;
         moveY = 0;
-        rb = transform.GetChild(0).GetComponent<Rigidbody>();
-        jumpDirection = new Vector3(0, 1, 0); 
+        rb = transform.GetComponent<Rigidbody>();
+        jumpDirection = new Vector3(0, 1, 0);
+
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -29,15 +35,49 @@ public class PlayerController : MonoBehaviour
 
     private void FixedUpdate()
     {
-        Vector3 move = new Vector3(moveX, 0, moveY).normalized;
-        rb.AddForce(move * acceleration);
+        Vector3 move = new Vector3(moveX, 0, moveY).normalized * velocity;
+        Debug.Log(move); 
+        rb.velocity = move; 
+
+        if (moveX != 0 || moveY != 0)
+        {
+            anim.SetBool("walk", true);
+        }
+        else
+        {
+            anim.SetBool("walk", false);
+        }
+        Quaternion current = transform.rotation;
+        int desired_angle = 0;
+        if (moveX > 0 && moveY > 0) {
+            desired_angle = 45;
+         }
+        if (moveX == 0 && moveY > 0) desired_angle = 0;
+        if (moveX < 0 && moveY > 0) desired_angle = -45;
+        if (moveX < 0 && moveY == 0) desired_angle = -90;
+        if (moveX > 0 && moveY == 0) desired_angle = 90;
+        if (moveX < 0 && moveY < 0) desired_angle = -135;
+        if (moveX == 0 && moveY < 0) desired_angle = 180;
+        if (moveX > 0 && moveY < 0) desired_angle = 135;
+
+        Quaternion desired_rotation = Quaternion.Euler(0, desired_angle, 0); 
+        if (moveX == 0 && moveY == 0)
+        {
+            desired_rotation = current; 
+        }
+        transform.rotation = Quaternion.Lerp(current, desired_rotation, Time.deltaTime * rotation_speed);
+        timeCount += Time.deltaTime; 
+
+
     }
 
     void OnMove(InputValue value)
     {
         Vector2 v = value.Get<Vector2>();
         moveX = v.x;
-        moveY = v.y; 
+        moveY = v.y;
+        
+
     }
 
     void OnJump() 
