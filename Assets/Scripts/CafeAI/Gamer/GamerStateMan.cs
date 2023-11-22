11803dabc71969b149b642e8c2c409c98f945e3e
Derek Jiang <derek.l.jiang@gmail.com>
2023-11-22 14:44:32 -0800

    Started Gamer AI

--- Assets/Scripts/CafeAI/Gamer/GamerStateMan.cs
+++ Assets/Scripts/CafeAI/Gamer/GamerStateMan.cs
@@ -2,17 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GamerStateMan : MonoBehaviour
+namespace CatCafeAI
 {
-    // Start is called before the first frame update
-    void Start()
+    public class GamerStateMan : StateMan
     {
-        
-    }
+        // Start is called before the first frame update
+        void Start()
+        {
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
     }
 }
